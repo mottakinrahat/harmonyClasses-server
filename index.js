a03e48354162432c34bfedc23d1d14d5887186de
mottakinrahat <rahatchowhdury661@gmail.com>
2023-06-09 03:10:20 +0600

    adminInstructorStudent

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors=require('cors');
 const port=process.env.PORT ||5000;
 
@@ -84,6 +84,13 @@ app.post('/users',async(req,res)=>{
     }
     const query={email: email}
     const result=await studentCollection.find(query).toArray();
+    res.send(result);
+   })
+
+   app.delete('/oneClass/:id',async(req,res)=>{
+    const id=req.params.id;
+    const query={_id:new ObjectId (id)}
+    const result=await studentCollection.deleteOne(query);
     res.send(result);
    })
 
