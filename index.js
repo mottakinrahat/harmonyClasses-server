83978d2da7b07bfd8ea937ccb68fe4cf07d10fd1
mottakinrahat <rahatchowhdury661@gmail.com>
2023-06-09 15:07:44 +0600

    DashBoardDone

--- index.js
+++ index.js
@@ -43,6 +43,34 @@ app.post('/users',async(req,res)=>{
   const result=await userCollection.insertOne(user);
   res.send(result)
 })
+
+app.get('/users',async(req,res)=>{
+  const result=await userCollection.find().toArray();
+  res.send(result);
+})
+
+app.patch('/users/admin/:id',async(req,res)=>{
+  const id=req.params.id;
+ const filter={_id:new ObjectId(id)};
+ const updateDoc={
+  $set:{
+    role:'admin'
+  },
+ }
+ const result=await userCollection.updateOne(filter, updateDoc);
+ res.send(result);
+})
+app.patch('/users/instructor/:id',async(req,res)=>{
+  const id=req.params.id;
+ const filter={_id:new ObjectId(id)};
+ const updateDoc={
+  $set:{
+    role:'instructor'
+  },
+ }
+ const result=await userCollection.updateOne(filter, updateDoc);
+ res.send(result);
+})
   
 
 //class related api
